6afa6d6881fe089140c9055013a8bff64d3d4349
acharayaP03 <acharyaP03@gmail.com>
2021-10-28 13:53:00 +1100

    update

--- 04_Updating_versions/terraform-manifests/c1-versions.tf
+++ 04_Updating_versions/terraform-manifests/c1-versions.tf
@@ -24,22 +24,3 @@ resource "azurerm_resource_group" "resource-group1" {
   location = "East US"
 }
 
-# Resource to create Random String
-resource "random_string" "myrandome" {
-  length = 16
-  special = false
-  upper = false
-}
-
-resource "azurerm_storage_account" "mysa" {
-  name                     = "mysa${random_string.myrandome.id}"
-  resource_group_name      = azurerm_resource_group.resource-group1.name
-  location                 = azurerm_resource_group.resource-group1.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-#   not available in v 2 above
-#   account_encryption_source  = "Microsoft.Storage"
-  tags = {
-    environment = "staging"
-  }
-}
